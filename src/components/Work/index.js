6d34569a79ccce53454c395f72e4ee9f07bb1d33
Drew Speed <drewjspeed@gmail.com>
2022-05-26 13:29:17 -0500

    added recent work

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,69 +1,63 @@
 import './index.scss'
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
 
-function Work() {
-    const form = useRef();
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs
-            .sendForm(
-                'service_5au0kpg',
-                'template_r7mjs37',
-                form.current,
-                'n9WQKxS1uwBdltToP'
-            ).then(
-                (result) => {
-                    console.log(result.text);
-                alert('Message successfully sent!');
-                window.location.reload(false);
-            }, (error) => {
-                console.log(error.text);
-                alert('Failed to send message. Please try again.');
-            }); 
-    }
+import IMG1 from '../../assets/images/bountybook.png';
+import IMG2 from '../../assets/images/notwork.png';
+import IMG3 from '../../assets/images/super-song-search.png'
 
 
+
+
+const Work = () => {
+    const data = [
+        {
+            id: 1,
+            image: IMG1,
+            title: 'Bounty Book',
+            github: 'https://github.com/Atticus-Robinson/Bounty_Book',
+            demo: 'https://bountybook.herokuapp.com/'
+        },
+        {
+            id: 2,
+            image: IMG2,
+            title: 'NotWork',
+            github: 'https://github.com/DrewSpeed/NotWork',
+            demo: 'https://fast-wave-33945.herokuapp.com/'
+        },
+        {
+            id: 3,
+            image: IMG3,
+            title: 'Super Song Search',
+            github: 'https://github.com/Jasonwesleysmith/super-song-search',
+            demo: 'https://jasonwesleysmith.github.io/super-song-search/'
+        },
+    
+    ]
     
     return (
         <>
         
         <section className="section-wrap">
-            <div className='text-zone'>
-            <h2 className="contact-header">Contact Me</h2>
-             <div className='contact-form'>
-                 <form ref={form} onSubmit={sendEmail}>
-                     <ul>
-                         <li className='half'>
-                             <input type='text' name='name' placeholder='Name' required />
-                         </li>
-                         <li className='half'>
-                             <input type='email' name='email' placeholder='Email' required />
-                         </li>
-                         <li>
-                             <input type='text' name='subject' placeholder='Subject' required />
-                         </li>
-                         <li>
-                             <textarea placeholder="Message" name="message" required ></textarea>
-                         </li>
-                         <li>
-                             <input type='submit' className='flat-button' value='SEND' />
-                         </li>
-                     </ul>
-                 </form>
-             </div>
+            <div className="portfolio-container">
+            <h2 className="contact-header">My Recent Work</h2>
+                <div className='items-container'>
+                {
+                    data.map(({id, image, title, github, demo}) => {
+                    return (
+                        <article key={id} className='portfolio-item'>
+                        <div className='portfolio-item-img'>
+                        <img src={image} alt={title} />
+                        </div>
+                            <h3>{title}</h3>
+                            <a href={github} className='btn'>Github</a>
+                            <a href={demo} rel='noreferrer' className='btn btn-primary' target='_blank'>Live Demo</a>
+                        </article>
+                    )
+                    })
+                }
+
+                </div>
             </div>
-            <div className='info-map'>
-                Drew Speed
-                <br />
-                Antioch TN, 37013
-                <br />
-                <span>drewjspeed@gmail.com<br />615.651.3338</span>
-                <p>I am interested in both freelance and contracted opportunities--especially unique and creative projects.  However, if you have any other requests or questions, please feel free to contact me using the form below.
-                </p>
-            </div>
+             
 
         </section>
         </>
